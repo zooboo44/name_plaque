4dee67c6006f8402a01533aa1001961b95898767
Christian Ziobro <ziobro44@gmail.com>
2026-09-12 16:09:39 -0700

    persist login token between program execution. write authentication record to file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from graph import Graph
+from pathlib import Path
 import asyncio
 
-grpah : Graph = Graph(".env")
+graph: Graph = Graph(".env")
+
+if not Path(".auth_record").exists():
+    graph.authenticate()
 
 async def main():
-    await grpah.print_presence()
+    await graph.print_presence()
 
 if __name__ == '__main__':
     asyncio.run(main())
