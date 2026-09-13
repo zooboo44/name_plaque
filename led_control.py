43f8956f56d6cb59f3db1f617ec3d0313ec0393d
Christian Ziobro <ziobro44@gmail.com>
2026-09-12 22:17:25 -0700

    fix neopixel parameters

--- led_control.py
+++ led_control.py
@@ -16,10 +16,10 @@ presence_color_matrix = {
 }
 
 COLOR = {
-    "RED" : (255, 0, 0),
+    "RED" : (255,0,0),
     "GREEN" : (0,255,0),
-    "YELLOW" : (251, 180, 43),
-    "PURPLE" : (125, 69, 119),
+    "YELLOW" : (255,255,0),
+    "PURPLE" : (125,69,119),
     "OFF" : (0,0,0)
 }
 
@@ -27,14 +27,20 @@ class LedControl():
     def __init__(self, env_file=".env"):
         load_dotenv(env_file)
 
-        self.led_length = os.getenv("LED_LENGTH")
+        self.led_length = int(os.getenv("LED_LENGTH"))
         self.led_pin = int(os.getenv("LED_PIN"))
-        self.led_brightness = os.getenv("LED_BRIGHTNESS")
+        self.led_brightness = float(os.getenv("LED_BRIGHTNESS"))
 
-        self.strip = neopixel.NeoPixel(board.D18, self.led_length, self.led_brightness, auto_write=True)
+        self.strip = (neopixel.NeoPixel(
+            board.D18,
+            self.led_length,
+            self.led_brightness,
+            auto_write=True,
+            pixel_order=neopixel.GRBW
+        ))
 
     def set_color(self, color):
         led_color = presence_color_matrix[color]
         print(led_color)
-        self.strip.fill(led_color)
-        pass
+        print(COLOR[led_color])
+        self.strip.fill(COLOR[led_color])
